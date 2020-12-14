468b77d3588748757c743fa1d655c35dee8e41b7
Pinhalito <pinhalito@gmail.com>
2020-12-14 12:00:08 -0300

    Create xtdc_2020_atualiza.sh

---
+++ xtdc_2020_atualiza.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#######################
+#    ^...^  `^...^´   #
+#   / o,o \ / O,O \   #
+#   |):::(| |):::(|   #
+# ====" "=====" "==== #
+#         TdC         #
+#      1998-2020      #
+#######################
+# Toca das Corujas
+# Códigos Binários, Funções de Onda e Teoria do Orbital Molecular Inc.
+# Unidade Barão Geraldo CX
+
